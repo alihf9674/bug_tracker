b034b3ed7a8af97c448741a4204fc24180d26c4e
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-04-21 21:32:19 +0430

    Initial commit

---
+++ src/Helpers/Config.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Exceptions\CofigFileNotFoundException;
+
+class Config
+{
+      public static function getFileContents($file_name)
+      {
+            $filePath = realpath(__DIR__ . '/../configs/' . $file_name . '.php');
+            if (!file_exists($filePath))
+                  throw new CofigFileNotFoundException();
+            return require $filePath;
+      }
+      public static function get(string $file_name, string $key = null)
+      {
+            $fileContents = self::getFileContents($file_name);
+            if (is_null($key))
+                  return $fileContents;
+            return $fileContents[$key] ?? null;
+      }
+}
