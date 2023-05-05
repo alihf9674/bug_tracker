74f556e2369a3a15114a5771b3bf2923bc952c68
alihf9674 <87331595+alihf9674@users.noreply.github.com>
2023-05-05 18:22:49 +0430

    Add test to update data

--- src/Database/PDOQueryBuilder.php
+++ src/Database/PDOQueryBuilder.php
@@ -2,12 +2,15 @@
 
 namespace App\Database;
 
+use PDO;
 use App\Contracts\DatabaseConnectionInterface;
 
 class PDOQueryBuilder
 {
       protected $table;
       protected $connection;
+      protected $conditions;
+      protected $values;
 
       public function __construct(DatabaseConnectionInterface $connection)
       {
@@ -30,5 +33,33 @@ class PDOQueryBuilder
             $query = $this->connection->prepare($sql);
             $query->execute(array_values($data));
             return (int)$this->connection->lastInsertId();
+      }
+
+      public function where(string $column, string $value)
+      {
+            $this->conditions[] = "{$column}=?";
+            $this->values[] = $value;
+            return $this;
+      }
+      public function update(array $data)
+      {
+            $fields = [];
+            foreach ($data as $column => $value) {
+                  $fields[] = "{$column}='{$value}'";
+            }
+            $fields = implode(', ', $fields);
+            $conditions = implode(' AND ', $this->conditions);
+            $sql = "UPDATE {$this->table} SET {$fields} WHERE {$conditions}";
+            $query = $this->connection->prepare($sql);
+            $query->execute($this->values);
+            return $query->rowCount();
+      }
+      public function truncateAllTable()
+      {
+            $query = $this->connection->prepare("SHOW TABLES");
+            $query->execute();
+            foreach ($query->fetchAll(PDO::FETCH_COLUMN) as $table) {
+                  $this->connection->prepare("TRUNCATE TABLE `{$table}`")->execute();
+            }
       }
 }
--- tests/Unit/PDOQueryBuilderTest.php
+++ tests/Unit/PDOQueryBuilderTest.php
@@ -9,22 +9,47 @@ use App\Database\PDODatabaseConnection;
 
 class PDOQueryBuilderTest extends TestCase
 {
-      public function testItCanCreateData()
+      private $queryBuilder;
+
+      public function setUp(): void
       {
             $pdoConnection = new PDODatabaseConnection($this->getConfig());
-            $queryBuilder = new PDOQueryBuilder($pdoConnection->connect());
+            $this->queryBuilder = new PDOQueryBuilder($pdoConnection->connect());
+            parent::setUp();
+      }
+      public function testItCanCreateData()
+      {
+            $result = $this->InserIntoDb();
+            $this->assertIsInt($result);
+            $this->assertGreaterThan(0, $result);
+      }
+      public function testItCanUpdateData()
+      {
+            $this->inserIntoDb();
+            $result = $this->queryBuilder
+                  ->table('bugs')
+                  ->where('user', 'User Name')
+                  ->where('email', 'EmailAddress@gmail.com')
+                  ->update(['email' => 'useremail@gmail.com', 'name' => 'User name']);
+            $this->assertEquals(1, $result);
+      }
+      private function InserIntoDb()
+      {
             $data = [
                   'name' => 'First Bug Report',
                   'link' => 'http://link.com',
                   'user' => 'User Name',
                   'email' => 'EmailAddress@gmail.com'
             ];
-            $result = $queryBuilder->table('bugs')->create($data);
-            $this->assertIsInt($result);
-            $this->assertGreaterThan(0, $result);
+            return $this->queryBuilder->table('bugs')->create($data);
       }
       private function getConfig()
       {
             return Config::get('database', 'pdo_testing');
+      }
+      public function tearDown(): void
+      {
+            $this->queryBuilder->truncateAllTable();
+            parent::tearDown();
       }
 }
